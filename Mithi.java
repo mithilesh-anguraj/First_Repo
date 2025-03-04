98b871819b8550172e7fa35af8b10d2846fd087e
Mithilesh Anguraj <2392760@cognizant.com>
2025-03-04 14:14:50 +0530

    First Commit

---
+++ Mithi.java
@@ -0,0 +1,7 @@
+import java .util.*;
+
+public class Mithi{
+  public static void main(String[] args){
+    System.out.println("Hello world");
+  }
+}
---
+++ sed/cal.sh
@@ -0,0 +1,6 @@
+echo "Num 1"
+read num1
+echo "Num 2"
+read num2
+let "sum=$((num1*num2))"
+echo "total:$sum"
